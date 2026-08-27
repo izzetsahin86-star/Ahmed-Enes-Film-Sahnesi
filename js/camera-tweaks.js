5d3be90ec14afb669616b46c7b6f9df8f080de5c
İzzet Şahin <izzetsahin86@icloud.com>
2026-08-27 12:52:02 +0300

    Eski kamera tweak modülünü kaldır

--- js/camera-tweaks.js
+++
@@ -1,7 +0,0 @@
-const href='./camera-tweaks.css';
-if(!document.querySelector(`link[href="${href}"]`)){
-  const link=document.createElement('link');
-  link.rel='stylesheet';
-  link.href=href;
-  document.head.append(link);
-}
